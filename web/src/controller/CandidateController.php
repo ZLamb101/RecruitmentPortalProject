b33d1a88474194878c04eea4afac3c56e51c0887
SLAIAX <1burgerboys@gmail.com>
2019-03-13 23:02:00 +1300

    Controller class files added (no methods currently)

---
+++ web/src/controller/CandidateController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1burg
+ * Date: 3/13/2019
+ * Time: 10:59 PM
+ */
+
+namespace bjz\portal\controller;
+
+
+class CandidateController extends Controller
+{
+
+}
