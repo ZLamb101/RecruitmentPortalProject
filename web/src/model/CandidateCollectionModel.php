94f5914d596fe21e598542666c1f03b0c86f3577
Bupton64 <bupton@hotmail.co.nz>
2019-03-15 10:41:49 +1300

    CandidateCollectionModel first implementation.

---
+++ web/src/model/CandidateCollectionModel.php
@@ -0,0 +1,63 @@
+<?php
+namespace bjz\portal\model;
+/**
+ * Class CandidateCollectionModel
+ *
+ * A collection model for candidates.
+ *
+ * @package bjz/portal
+ */
+class CandidateCollectionModel extends Model
+{
+    /**
+     * @var array, the ID's of the candidates collected
+     */
+    private $candidate_ids;
+    /**
+     * @var int, the number of candidates collected
+     */
+    private $N;
+
+    /**
+     * @return int, the number of candidates collected
+     */
+    public function getN()
+    {
+        return $this->N;
+    }
+
+    /**
+     * Constructor
+     *
+     * Handles multiple use cases.
+     * Short List use case - gets all candidates based on the short list ID.
+     * Search use case - gets all candidates based on some search criteria
+     */
+    public function __construct($id, $search_str="")
+    {
+        if($search_str == ""){ // Short List use case
+            if (!$result = $this->db->query("SELECT * FROM `candidate` LEFT JOIN
+                                             ON `candidate`.`id` = `short_list`.`owner_id`
+                                             WHERE `short_list`.`id` = '$id';")){
+                $this->candidate_ids = array_column($result->fetch_all(), 0);
+                $this->N = $result->num_rows;
+            }
+        } else { // Search use case
+            //TODO Implement this
+        }
+    }
+
+    /**
+     * Get candidate collection
+     * Get the candidates in the collection
+     * @return \Generator|CandidateModel[]
+     */
+    public function getProducts()
+    {
+        foreach ($this->candidate_ids as $id) {
+            // Use a generator to save on memory/resources
+            // load accounts from DB one at a time only when required
+            yield (new CandidateModel())->load($id);
+        }
+    }
+}
